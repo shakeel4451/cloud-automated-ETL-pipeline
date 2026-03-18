520cb187c09c0265bed19fe6d77517f8022166f8
Ahmad <mshakeel44514451@gmail.com>
2026-03-19 00:00:19 +0500

    function named 'setup_database' is made to setup database, connection is established also

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ import requests
 import sqlite3
 import pandas as pd
 from datetime import datetime
+
+DB_file="tech_news.db"
+EXPORT_FILE = "daily_report.csv"
+
+def setup_database():
+  """Initializes the SQLite database and schema."""
+  conn=sqlite3.connect(DB_file)
+  cursor=conn.cursor()
+
+  
